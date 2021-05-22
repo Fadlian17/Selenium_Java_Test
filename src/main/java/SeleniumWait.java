39b7a42db1e26c46b6242c67016d9ee16bd118c5
Fadlian17 <alfansyahfadlian@gmail.com>
2021-05-22 13:27:06 +0700

    Selenium Java Awaits By jUnit5

---
+++ src/main/java/SeleniumWait.java
@@ -0,0 +1,56 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SeleniumWait {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUpDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void checkGithubSearch(){
+        driver.get("https://github.com/");
+
+        WebElement searchInput = driver.findElement(By.cssSelector("[name='q']"));
+
+        String searchPhrase = "selenium";
+        searchInput.sendKeys(searchPhrase);
+        searchInput.sendKeys(Keys.ENTER);
+
+        List<String> actualItems = driver.findElements(By.cssSelector(".repo-list-item")).stream()
+                .map(element -> element.getText().toLowerCase())
+                .collect(Collectors.toList());
+
+        List<String> expectedItems = actualItems.stream()
+                .filter(item ->item.contains(searchPhrase))
+                .collect(Collectors.toList());
+
+        System.out.println(LocalDateTime.now());
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[title='invalid title']")).isDisplayed());
+        Assertions.assertEquals(expectedItems,actualItems);
+    }
+
+    @AfterAll
+    public static void tearDownDriver() {
+        System.out.println(LocalDateTime.now());
+        driver.quit();
+    }
+
+}
